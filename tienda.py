f4bdd0daf84465ca0785e771a09a6d59ed5ef0f0
David Carrizo <dv.jcarrizo@gmail.com>
2024-08-26 21:57:44 -0400

    Actualizado

--- tienda.py
+++ tienda.py
@@ -17,11 +17,6 @@ class Tienda():
     @property
     def envio(self):
         return self.__envio    
-        
-    @property
-    def stock_producto(self):
-        return(f' Nombre del producto: {self.producto}'
-               f' El costo de envío es: {self.envio}')
 
     def NuevoProducto(self, producto: Producto):
         for p in self.__productos:
@@ -56,33 +51,26 @@ class Supermercado(Tienda):
                     "Nombre producto \tPrecio \t\t Cantidad\n")
             stock_msg = f"Pocos productos disponibles ({p.stock})" if p.stock < 10 else f"Stock: {p.stock}"
             result.append(f"{retorno}{p.nombre.title()} \t\t\t{p.precio} \t\t {stock_msg}")
-        return '\n'.join(result)
+        return f'{retorno}\n'.join(result)
     
 
 class Farmacia(Tienda):
     def ListaProductos(self):
         result = []
-        for p in self._Tienda__productos:
-            retorno = ("::::::::Lista de Productos::::::::\n"
-                    "Nombre producto \tPrecio \t\t Promoción\n")            
+        retorno = ("\n::::::::Lista de Productos::::::::\n"
+        "Nombre producto \tPrecio \t\t Promoción\n") 
+        for p in self._Tienda__productos:           
             envio_msg = "Envío gratis al solicitar este producto" if p.precio > 15000 else "El producto no posee promoción"
             result.append(f"{retorno}{p.nombre.title()} \t\t\t{p.precio} \t\t {envio_msg}")
-        return '\n'.join(result)
+        return f'{retorno}\n'.join(result)
 
     def RealizarVenta(self, nombre_producto, cantidad):
         if cantidad > 3:
             return False
-        elif cantidad <= 3:    
-            for p in self._Tienda__productos:
-                if p.nombre == nombre_producto:
-                    if isinstance(self, Farmacia):
-                        return True  
-                    elif p.stock >= cantidad:
-                        p.stock = p.stock - cantidad
-                        return True
-                    else:
-                        return False  
+        return super().RealizarVenta(nombre_producto, cantidad)
         
     
 class Restaurante(Tienda):
-    pass
+    def RealizarVenta(self, nombre_producto, cantidad):
+        return True
+
